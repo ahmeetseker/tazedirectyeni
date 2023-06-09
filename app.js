e8912d12f9747ad04ea175ea1c94f8a9cac32be3
ahmetseker <ahmetseker1905@hotmail.com.tr>
2023-06-09 17:22:50 +0300

    update-tabs

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const countDate = new Date('').getTime();
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontents = document.getElementsByClassName("tabcontent");
 
-function harvest () {
-    
+
+
+
+function openTab(evt, tabName) {
+  // Aktif sekmenin sınıfını sil
+
+
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+
+
+
+  // Tüm içerikleri gizle
+  for (var i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
+  }
+
+
+
+
+
+  // Tıklanan sekmenin sınıfını ayarla          
+  evt.currentTarget.classList.add("active");
+
+  
+
+  // Gösterilecek içeriği belirle
+  document.getElementById(tabName).style.display = "block";
 }
+
+// Varsayılan olarak ilk sekme açık olsun
+tablinks[0].classList.add("active");
+document.getElementById("tab1").style.display = "block";  
+
+
+
+
+
+
+
