29c6bcdb2fc4a08a568479143efad00b446c3b3d
ahmetseker <ahmetseker1905@hotmail.com.tr>
2023-06-12 18:42:16 +0300

    UPDATE

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
 var tablinks = document.getElementsByClassName("tablinks");
 var tabcontents = document.getElementsByClassName("tabcontent");
 
-
-
+// Varsayılan olarak ilk sekme açık olsun
+tablinks[0].classList.add("active");
+document.getElementById("tab1").style.display = "block";
 
 function openTab(evt, tabName) {
-  // Aktif sekmenin sınıfını sil
-
-
   for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
   }
-
-
-
-
-  // Tüm içerikleri gizle
+  
   for (var i = 0; i < tabcontents.length; i++) {
     tabcontents[i].style.display = "none";
   }
 
-
-
-
-
-  // Tıklanan sekmenin sınıfını ayarla          
   evt.currentTarget.classList.add("active");
-
-  
-
-  // Gösterilecek içeriği belirle
   document.getElementById(tabName).style.display = "block";
 }
 
-// Varsayılan olarak ilk sekme açık olsun
-tablinks[0].classList.add("active");
-document.getElementById("tab1").style.display = "block";  
+function toggleTabContent(tabName) {
+  var rightArrows = document.getElementsByClassName("rightArrow");
 
+  for (var i = 0; i < rightArrows.length; i++) {
+    rightArrows[i].getElementsByTagName("i")[0].style.display = "none";
+  }
 
-
-
-
-
-
+  var selectedTab = document.getElementById(tabName);
+  var rightArrow = selectedTab.getElementsByClassName("rightArrow")[0];
+  rightArrow.getElementsByTagName("i")[0].style.display = "block";
+}
