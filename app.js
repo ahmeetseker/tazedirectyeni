8e7490fb8f7559704b5df95cba02f094c15ad62b
ahmet <ahmetseker1905@hotmail.com.tr>
2023-06-17 13:50:37 +0300

    update

--- app.js
+++ app.js
@@ -6,31 +6,27 @@ var rightArrows = document.getElementsByClassName("rightArrow");
 tablinks[0].classList.add("active");
 document.getElementById("tab1").style.display = "block";
 
-
 function openTab(evt, tabName) {
+  var targetTab;
+  if (typeof tabName === "string") {
+    targetTab = document.getElementById(tabName);
+  } else {
+    var selectedOption = evt.target.value;
+    targetTab = document.getElementById(selectedOption);
+  }
+
   for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
     tablinks[i].querySelector("button").style.color = ""; 
     rightArrows[i].style.display = "none";
-   
   }
 
   for (var i = 0; i < tabcontents.length; i++) {
     tabcontents[i].style.display = "none";
   }
 
-
-
-
-  document.getElementById(tabName).style.display = "block";
+  targetTab.style.display = "block";
   evt.currentTarget.parentElement.classList.add("active");
   evt.currentTarget.parentElement.nextElementSibling.style.display = "block";
-
-
-evt.currentTarget.style.color = "black";
-
-
-
-
-
+  evt.currentTarget.style.color = "black";
 }
